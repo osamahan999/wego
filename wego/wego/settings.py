52360673c485d65fd5849911f950743b30eae40c
Osama Hanhan <osamahanhan@Osamas-MacBook-Pro.local>
2023-12-23 15:16:35 -0800

    Dot env setup

--- wego/wego/settings.py
+++ wego/wego/settings.py
@@ -11,19 +11,26 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# env secrets setup
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4h0dv@x_mgz#+bxxutwke&vrr%vixz^!cg+!&c3)aqi0+9m3n5'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG_MODE")
 
 ALLOWED_HOSTS = []
 
