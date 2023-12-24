61be1e578a916524e4d5d2ae4927b9e315e7f556
Osama Hanhan <osamahanhan@Osamas-MacBook-Pro.local>
2023-12-23 16:05:56 -0800

    fix env stuff

--- wego/wego/settings.py
+++ wego/wego/settings.py
@@ -13,15 +13,14 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 
-import dotenv
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # env secrets setup
 dotenv_file = os.path.join(BASE_DIR, ".env")
-if os.path.isfile(dotenv_file):
-    dotenv.load_dotenv(dotenv_file)
+load_dotenv()
 
 
 # Quick-start development settings - unsuitable for production
