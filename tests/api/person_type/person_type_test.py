ace4a45086971008b563c406a7c4c1fce348a280
Osama Hanhan <osamahanhan@Osamas-MacBook-Pro.local>
2023-12-29 15:35:36 -0800

    added graphql tests

--- tests/api/person_type/person_type_test.py
+++ tests/api/person_type/person_type_test.py
@@ -1,10 +1,37 @@
-from unittest import TestCase
+from django.test import Client as DjangoTestClient, RequestFactory, TransactionTestCase
+from graphene.test import Client as GrapheneClient
 
 from api.person_type.person_type import PersonType
+from api.schema import schema
+from immutable_entities.immutable_event import immutable_event
 from immutable_entities.immutable_person import ImmutablePerson
+from person.models import Person
+from tests.seeder import Seeder
 
 
-class PersonTypeTest(TestCase):
+class PersonTypeTest(TransactionTestCase):
+    USERNAME: str = "osama"
+    PASSWORD: str = "osama"
+    EMAIL: str = "osama@gmail.com"
+
+    client: DjangoTestClient = None
+    graphql_client: GrapheneClient = None
+    user: Person = None
+
+    def setUp(self) -> None:
+        self.client = DjangoTestClient()
+        self.user = Person.objects.create_user(
+            username=self.USERNAME, password=self.PASSWORD, email=self.PASSWORD
+        )
+
+        self.client.login(username=self.USERNAME, password=self.PASSWORD)
+
+        request_factory = RequestFactory()
+        request = request_factory.post("/graphql/")
+        request.user = self.user
+
+        self.graphql_client = GrapheneClient(schema, context=request)
+
     def test_get_person_type(self):
         key: int = 1
         username = "username"
@@ -34,3 +61,78 @@ class PersonTypeTest(TestCase):
         )
 
         self.assertEqual(person.to_person_type(), expected_person_type)
+
+    def test_get_me(self):
+        query: str = """
+            query {
+                me {
+                    id,
+                    username,
+                    firstName,
+                    lastName,
+                    email,
+                    phoneNumber
+                }
+            }
+        """
+        result = self.graphql_client.execute(query)
+
+        assert not result.get("errors")
+
+        self.assertEqual(result.get("data").get("me").get("username"), self.USERNAME)
+        self.assertEqual(result.get("data").get("me").get("firstName"), "")
+
+    def test_get_owned_events(self):
+        events = Seeder.seed_events(
+            [
+                immutable_event.ImmutableEvent.ImmutableEventBuilder()
+                .set_owner(self.user.transform_to_immutable_entity())
+                .set_name("name")
+                .set_description("description")
+                .set_location("location")
+                .set_latitude(3.0)
+                .set_longitude(4.0)
+                .set_attendees([self.user.transform_to_immutable_entity()])
+                .build(),
+                immutable_event.ImmutableEvent.ImmutableEventBuilder()
+                .set_owner(self.user.transform_to_immutable_entity())
+                .set_name("name2")
+                .set_description("description2")
+                .set_location("location2")
+                .set_latitude(3.0)
+                .set_longitude(4.0)
+                .set_attendees([self.user.transform_to_immutable_entity()])
+                .build(),
+            ]
+        )
+
+        query: str = """
+            query {
+                me {
+                    id,
+                    ownedEvents {
+                        edges {
+                            node {
+                                id,
+                                name
+                            }
+                        }
+                    }
+                }
+            }
+        """
+
+        result = self.graphql_client.execute(query)
+
+        self.assertIsNone(result.get("errors"))
+        self.assertEqual(result.get("data").get("me").get("id"), self.user.id)
+
+        nodes = [
+            result.get("node")
+            for result in result.get("data").get("me").get("ownedEvents").get("edges")
+        ]
+        event_1 = [node for node in nodes if node.get("name") == events[0].name][0]
+        event_2 = [node for node in nodes if node.get("name") == events[1].name][0]
+
+        self.assertIsNotNone(event_1)
+        self.assertIsNotNone(event_2)
---
+++ tests/seeder/seeder.py
@@ -0,0 +1,45 @@
+from chat_group.models import ChatGroup, StatusEnum
+from event.models import Event, EventAttendee
+from immutable_entities.immutable_event import immutable_event
+
+
+class Seeder:
+    @staticmethod
+    def seed_events(
+        immutable_events: list[immutable_event.ImmutableEvent],
+    ) -> list[Event]:
+        chat_groups_to_create = [
+            ChatGroup(status=StatusEnum.DRAFT) for _ in immutable_events
+        ]
+
+        chat_groups = ChatGroup.objects.bulk_create(chat_groups_to_create)
+
+        events_to_create: list[Event] = [
+            Event(
+                name=immutable_events[i].name,
+                description=immutable_events[i].description,
+                location=immutable_events[i].location,
+                latitude=immutable_events[i].latitude,
+                longitude=immutable_events[i].longitude,
+                owner_id=immutable_events[i].owner.key,
+                chat_group_id=chat_groups[i].id,
+            )
+            for i in range(len(immutable_events))
+        ]
+
+        # preserves original order
+        events = Event.objects.bulk_create(events_to_create)
+
+        key_to_event: dict[int, list] = {
+            events[i].id: [p.key for p in immutable_events[i].attendees]
+            for i in range(len(events))
+        }
+
+        event_attendees = [
+            EventAttendee(event_id=i, person_id=j)
+            for i in key_to_event.keys()
+            for j in key_to_event[i]
+        ]
+
+        EventAttendee.objects.bulk_create(event_attendees)
+        return events
